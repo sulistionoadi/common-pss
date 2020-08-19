afb4a35d65cae6ed28f14f935c84df9c25d929f7
Adi Sulistiono <sulistiono.adi@gmail.com>
2020-08-19 09:50:04 +0700

    create method buildDefaultFilter

--- src/main/java/com/sulistionoadi/ngoprek/common/pss/helper/PssHelper.java
+++ src/main/java/com/sulistionoadi/ngoprek/common/pss/helper/PssHelper.java
@@ -8,6 +8,23 @@ import com.sulistionoadi.ngoprek.common.pss.dto.PssFilter;
 
 public class PssHelper {
 
+	public static PssFilter buildDefaultFilter() {
+		PssFilter filter = new PssFilter();
+		filter.setLength(10);
+		filter.setStart(0);
+		
+		HashMap<String, String> order = new HashMap<>();
+		order.put(PssConstant.PSS_ORDER_COLUMN, "0");
+		order.put(PssConstant.PSS_ORDER_DIRECTION, "asc");
+		filter.setOrder(Arrays.asList(order));
+		
+		HashMap<String, String> search = new HashMap<>();
+		search.put(PssConstant.PSS_SEARCH_VAL, null);
+		search.put(PssConstant.PSS_SEARCH_REG, "false");
+		filter.setSearch(search);
+		return filter;
+	}
+	
 	public static PssFilter buildSelect2Filter(String q, Integer page, Integer sortIdx, String sortDir) {
 		PssFilter filter = new PssFilter();
 		filter.setLength(10);
